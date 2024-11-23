fb2e492e550a9107abecaa2a53b9a07acf2b1b2c
GitHub <actions@github.com>
2024-11-23 23:54:57 +0600

    problem 03 solve

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 // Problem-01 : Tax Calculator
 function calculateTax(income, expenses) {
-    if(income < 0 || expenses < 0 || income < expenses){
-        return "Invalid Input";
-    }else{
-        return (income - expenses) * .20;
-    }
+  if (income < 0 || expenses < 0 || income < expenses) {
+    return "Invalid Input";
+  } else {
+    return (income - expenses) * 0.2;
+  }
 }
 const result = calculateTax(10000, 3000);
 // console.log(result);
 
-
 // Problem 02 : Notification Generator
 function sendNotification(email) {
-    if(!email.includes('@')){
-        return "Invalid Email";
-    }
-    const text = email.split('@');
-    const userName = text[0];
-    const domainName = text[1];
-    return `${userName} sent you an email from ${domainName}`;
+  if (!email.includes("@")) {
+    return "Invalid Email";
+  }
+  const text = email.split("@");
+  const userName = text[0];
+  const domainName = text[1];
+  return `${userName} sent you an email from ${domainName}`;
 }
-console.log(sendNotification('farhan34@yahoo.com'));
+// console.log(sendNotification("farhan34@yahoo.com"));
+
+// Problem-03: Checking Digits Inside a Name
+function checkDigitsInName(name) {
+    if (typeof name !== 'string') {
+        return "Invalid Input";
+    }
+    let character = false;
+    for (let i = 0; i < name.length; i++) {
+        if (name[i] >= 0 && name[i] <= 9) {
+            character = true;
+            break;
+        }
+    }
+    return character;
+}
+const result2 = checkDigitsInName('hello3');
+console.log(result2);
+
+
+
+
+
+
+
